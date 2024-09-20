6c877da1f86a724a00bc0600a5acd6453bd27fba
codebypaulina <paulinaczeschla@web.de>
2024-09-20 17:09:48 +0200

    task: read data

---
+++ db/models/Place.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const placeSchema = new Schema({
+  // _id: { type: mongoose.Schema.Types.ObjectId },
+
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  image: { type: String, required: true },
+  mapURL: { type: String, required: true },
+  description: { type: String, required: true },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId, // Referenz auf Kommentardokumente
+      ref: "Comment", // das Modell noch erstellen!
+    },
+  ],
+});
+
+const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
+
+export default Place;
--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,23 +1,38 @@
-import { db_places } from "../../../../lib/db_places";
-import { db_comments } from "../../../../lib/db_comments";
+// // import { db_places } from "../../../../lib/db_places";
+// // import { db_comments } from "../../../../lib/db_comments";
+import dbConnect from "../../../../db/connect";
+import Place from "../../../../db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
 
   if (!id) {
     return;
   }
 
-  const place = db_places.find((place) => place._id.$oid === id);
-  const comment = place?.comments;
-  const allCommentIds = comment?.map((comment) => comment.$oid) || [];
-  const comments = db_comments.filter((comment) =>
-    allCommentIds.includes(comment._id.$oid)
-  );
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
 
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
+    if (!place) {
+      return response.status(404).json({ status: "Place not found" });
+    }
+
+    response.status(200).json({ place: place });
   }
-
-  response.status(200).json({ place: place, comments: comments });
 }
+
+// const place = db_places.find((place) => place._id.$oid === id);
+// const comment = place?.comments;
+// const allCommentIds = comment?.map((comment) => comment.$oid) || [];
+// const comments = db_comments.filter((comment) =>
+//   allCommentIds.includes(comment._id.$oid)
+// );
+
+// if (!place) {
+//   return response.status(404).json({ status: "Not found" });
+// }
+
+// response.status(200).json({ place: place, comments: comments });
+// }
